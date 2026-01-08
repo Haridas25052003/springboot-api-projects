d80e3aed18e913bf21881ee6cd48fe18f6dcc548
Haridas250503 <haridasshinde314@gmail.com>
2026-01-09 02:28:29 +0530

    initial commit

---
+++ springboot-api-02/src/main/java/com/demo/SpringbootApi02Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApi02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApi02Application.class, args);
+	}
+
+}
